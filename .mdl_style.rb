bfdf2535371208bd8b36905c7908bb2a0ec700ed
P-manBrown <80106172+P-manBrown@users.noreply.github.com>
2022-12-26 14:09:37 +0900

    feat: add markdownlint style

---
+++ .mdl_style.rb
@@ -0,0 +1,10 @@
+# Style file for markdownlint
+all
+# exclude rules
+exclude_rule "MD002"
+exclude_rule "MD006"
+exclude_rule "MD013"
+exclude_rule "MD033"
+# override parameters
+rule "MD007", indent: 2
+rule "MD009", br_spaces: 2
