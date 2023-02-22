e79e72e78820e4dcc5b18eb4359d3ce12f8eaaf2
P-manBrown <80106172+P-manBrown@users.noreply.github.com>
2023-02-22 17:25:51 +0900

    feat!: change docker api service configuration settings

--- Docker/api/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-rm -f /home/ruby/myapp-backend/tmp/pids/server.pid
-
-exec "$@"
--- setup/config/puma.rb
+++ setup/config/puma.rb
@@ -1,10 +1,11 @@
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+threads_count = ENV.fetch("RAILS_MAX_THREADS", 5)
 threads threads_count, threads_count
+
 environment ENV.fetch("RAILS_ENV") { "development" }
-plugin :tmp_restart
 
-app_root = File.expand_path("../..", __FILE__)
+app_root = File.expand_path("..", __dir__)
 bind "unix://#{app_root}/tmp/sockets/puma.sock"
-
 stdout_redirect "#{app_root}/log/puma.stdout.log",
                 "#{app_root}/log/puma.stderr.log", true
+
+plugin :tmp_restart
